04365e2bcf825372d85515013c1afc794570b36e
adityachauhan0 <127509777+adityachauhan0@users.noreply.github.com>
2025-12-19 13:09:40 +0530

    Add startup banner to client application
    Added a banner to display at the start of the client.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,7 +7,17 @@ import (
 	"github.com/valhalla/go-torrent/pkg/client"
 )
 
+const banner = `
+   ____       _____                                  _ 
+  / ___| ___ |_   _|__  _ __ _ __ ___ _ __   | |_ 
+ | |  _ / _ \  | |/ _ \| '__| '__/ _ \ '_ \  | __|
+ | |_| | (_) | | | (_) | |  | | |  __/ | | | | |_ 
+  \____|\___/  |_|\___/|_|  |_|  \___|_| |_|  \__|
+                                                    
+`
+
 func main() {
+	fmt.Println(banner)
 	if len(os.Args) < 3 {
 		fmt.Println("Usage: client <torrent_file> <output_dir>")
 		os.Exit(1)
